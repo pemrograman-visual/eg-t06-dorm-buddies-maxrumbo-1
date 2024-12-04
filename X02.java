96c880fb12f93fa2aeb315423eefba11ecd6edd7
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-12-04 14:29:47 -0800

    PAIR 20

--- X02.java
+++ X02.java
@@ -1,2 +1,49 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24037-Maxwell Rumahorbo
+// 12S24030-Swasti Sihombing
+import java.util.*;
+import java.lang.Math;
+
+public class X02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] buddy = new String[4];
+        int i, n;
+
+        n = 0;
+        i = 0;
+        inisialisasi(buddy, i);
+        n = func_input(buddy, n);
+        output(buddy, n);
+    }
+    
+    public static void inisialisasi(String[] buddy, int i) {
+        for (i = 0; i <= 3; i++) {
+            buddy[i] = "";
+        }
+    }
+    
+    public static int func_input(String[] buddy, int n) {
+        int a;
+
+        n = 0;
+        for (a = 0; a <= 3; a++) {
+            buddy[a] = input.nextLine();
+            if (buddy[a].equals("---")) {
+                a = 4;
+            } else {
+                n = n + 1;
+            }
+        }
+        
+        return n;
+    }
+    
+    public static void output(String[] buddy, int n) {
+        int h;
+
+        for (h = 0; h <= n - 1; h++) {
+            System.out.println(buddy[h]);
+        }
+    }
+}
